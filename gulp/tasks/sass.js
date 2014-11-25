fd39f6174e60e8d841b22f6358006c70136d0c29
Eric BREHAULT <ebrehault@gmail.com>
2014-11-25 17:20:07 +0100

    sourcemaps + minify

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,14 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var minify = require('gulp-minify-css');
 var handleErrors = require('../util/handleErrors');
 var config = require('../config').sass;
 
 gulp.task('sass', function() {
     gulp.src(config.src)
+        .pipe(sourcemaps.init())
         .pipe(sass()
             .on('error', handleErrors)
         )
         .pipe(concat(config.outputName))
+        .pipe(minify())
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(config.dest));
 });
