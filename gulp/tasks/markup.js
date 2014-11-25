7c896b802acb0ded9f77631344dd8b099ecc5350
Paul Everitt <paul@agendaless.com>
2014-11-25 16:15:41 -0500

    Directory listing on top-level build when served. Refactor "html" into "hello", the simplest-possible demo of external application.

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var config = require('../config').markup
 
-gulp.task('markup', function() {
-  return gulp.src(config.src)
+gulp.task('markup', function () {
+  return gulp.src(config.src, {base: config.base})
     .pipe(gulp.dest(config.dest));
 });
