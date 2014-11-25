e82cb7f08cab0ecb7323b5bbb4e390f9095cd297
Paul Everitt <paul@agendaless.com>
2014-11-25 17:37:10 -0500

    Menu where sections can be registered with the $state and iterated over.

--- demos/hello-ui-router/app.js
+++ demos/hello-ui-router/app.js
@@ -3,12 +3,25 @@ function ModuleInit($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state("site.state1", {
              url: 'state1',
-             templateUrl: 'state1.partial.html'
+             templateUrl: 'state1.partial.html',
+             section: {
+               'title': 'State One'
+             }
            })
     .state("site.state2", {
              url: 'state2',
-             templateUrl: 'state2.partial.html'
+             templateUrl: 'state2.partial.html',
+             section: {
+               'title': 'State Two'
+             }
            })
+    .state("site.state3", {
+             url: 'state3',
+             templateUrl: 'state3.partial.html',
+             section: {
+               'title': 'State Three'
+             }
+           });
 }
 
 angular.module('hello-ui-router', ['moondash'])
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
+require('./controllers')
+
 function ModuleInit($stateProvider) {
   $stateProvider
     .state("site", {
              url: '/',
              abstract: true,
-             templateUrl: '/layout/mn-layout.partial.html'
+             templateUrl: '/layout/mn-layout.partial.html',
+             controller: 'LayoutCtrl as ctrl'
            })
 }
 
