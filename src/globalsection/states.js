430275d217cf3356a05494eee4dc8a9572062b4b
Paul Everitt <paul@agendaless.com>
2014-11-29 10:23:51 -0500

    The leading slash is important.

--- src/globalsection/states.js
+++ src/globalsection/states.js
@@ -1,10 +1,42 @@
 function ModuleInit($stateProvider) {
   $stateProvider
     .state('root.dashboard', {
-             url: 'dashboard',
+             url: '/dashboard',
              views: {
                'md-content@root': {
                  template: '<h2>Dashboard</h2>'
+               }
+             }
+           })
+    .state('root.settings', {
+             url: '/settiings',
+             views: {
+               'md-content@root': {
+                 template: '<h2>Settings</h2>'
+               }
+             }
+           })
+    .state('root.types', {
+             url: '/types',
+             views: {
+               'md-content@root': {
+                 template: '<h2>Types</h2>'
+               }
+             }
+           })
+    .state('root.types.people', {
+             url: '/types',
+             views: {
+               'md-content@root': {
+                 template: '<h2>People</h2>'
+               }
+             }
+           })
+    .state('root.types.invoices', {
+             url: '/invoices',
+             views: {
+               'md-content@root': {
+                 template: '<h2>Invoices</h2>'
                }
              }
            });
