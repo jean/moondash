f18629ed5f367d840a743e208130d33ad58d0815
Davide Moro <davide.moro@gmail.com>
2014-12-05 23:47:06 +0100

    karma point to a more specific test target (unit/*.spec.js)

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -3,11 +3,11 @@
 module.exports = function(karma) {
   karma.set({
     files: [
-      '../src/**/*.spec.js',
+      '../src/**/unit/*.spec.js',
     ],
     frameworks: ['jasmine', 'browserify'],
     preprocessors: {
-      '../src/**/*.spec.js': ['browserify']
+      '../src/**/unit/*.spec.js': ['browserify']
     },
     browsers: ['PhantomJS'],
     reporters: ['dots'],
