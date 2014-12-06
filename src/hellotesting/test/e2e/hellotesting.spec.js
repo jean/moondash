156d2e743c770a5025ec617f644f41c993f2ab41
Davide Moro <davide.moro@gmail.com>
2014-12-06 23:49:22 +0100

    added dummy e2e test

---
+++ src/hellotesting/test/e2e/hellotesting.spec.js
@@ -0,0 +1,9 @@
+describe('angularjs homepage', function() {
+  it('should greet the named user', function() {
+    browser.get('http://www.angularjs.org');
+
+    expect('1').toEqual('1');
+  });
+
+});
+
