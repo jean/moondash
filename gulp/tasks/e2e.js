9e682f1f9a3d7195a4daa1aa7fa6ef4eaa8cf6e8
Davide Moro <davide.moro@gmail.com>
2014-12-07 14:31:15 +0100

    added notes for next e2e testing setup tasks (we need to setup a browsersync instance specific for e2e testing)

--- gulp/tasks/e2e.js
+++ gulp/tasks/e2e.js
@@ -1,13 +1,16 @@
 'use strict';
 
-var gulp            = require('gulp');
-var protractor      = require('gulp-protractor').protractor;
+// TODO: setup browserSync (specific for e2e) for real e2e tests
+var gulp = require('gulp');
+var protractor = require('gulp-protractor').protractor;
 var webdriverUpdate = require('gulp-protractor').webdriver_update;
-var config          = require('../config');
+//var browserSync = require('browser-sync');
+var config = require('../config');
 
 gulp.task('webdriver-update', webdriverUpdate);
 
-gulp.task('e2e', ['webdriver-update'], function() {
+//gulp.task('e2e', ['webdriver-update', 'browserSync'], function(done) {
+gulp.task('e2e', ['webdriver-update'], function(done) {
 
   return gulp.src(config.e2e.specs)
     .pipe(protractor({
@@ -17,5 +20,9 @@ gulp.task('e2e', ['webdriver-update'], function() {
       // Make sure failed tests cause gulp to exit non-zero
       throw err;
     });
+//    .on('end', function () {
+//      browserSync.exit();
+//      done();
+//    });
 
 });
