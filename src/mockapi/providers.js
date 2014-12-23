278e7956df03c4cb65df02731765439920e9289f
Paul Everitt <paul@agendaless.com>
2014-12-23 16:59:10 -0500

    Slight refactoring of mockApi on way to larger.

--- src/mockapi/providers.js
+++ src/mockapi/providers.js
@@ -3,54 +3,64 @@
 var _ = require('lodash');
 
 function MockRest() {
+  var _this = this;
   this.mocks = {};
 
   this.$get = function () {
     var mocks = this.mocks;
     return {
-      registerMocks: function ($httpBackend) {
-        // Iterate over all the registered mocks and register them
-        _.map(mocks, function (moduleMocks) {
-          _(moduleMocks).forEach(function (mock) {
-            // Get the data from the mock
-            var method = mock.method || 'GET',
-              pattern = mock.pattern,
-              responder = mock.responder,
-              responseData = mock.responseData;
-
-            var wrappedResponder = function (method, url, data, headers) {
-
-              // If the mock says to authenticate and we don't have
-              // an Authorization header, return 401.
-              if (mock.authenticate) {
-                var authz = headers['Authorization'];
-                if (!authz) {
-                  return [401, {"message": "Login required"}];
-                }
-              }
-
-              // A generic responder for handling the case where the
-              // mock just wanted the basics and supplied responseData
-              if (!responder) {
-                return [200, responseData]
-              }
-
-              // Got here, so let's go ahead and call the
-              // registered responder
-              return responder(method, url, data, headers)
-            };
-
-            $httpBackend.when(method, pattern)
-              .respond(wrappedResponder);
-          });
-        });
-      }
+      registerMocks: registerMocks
     };
   };
 
   this.addMocks = function (k, v) {
     this.mocks[k] = v;
   };
+
+  function registerMocks($httpBackend) {
+    // Iterate over all the registered mocks and register them
+    _.map(_this.mocks, function (moduleMocks) {
+      _(moduleMocks).forEach(function (mock) {
+
+        // To register with $httpBackend's matchers, we need two things
+        // from the mock: the method and the URL pattern.
+        var method = mock.method || 'GET',
+          pattern = mock.pattern;
+
+        var wrappedResponder = function (method, url, data, headers) {
+          return dispatch(mock, method, url, data, headers);
+        };
+
+        $httpBackend.when(method, pattern)
+          .respond(wrappedResponder);
+      });
+    });
+  }
+
+  function dispatch(mock, method, url, data, headers) {
+    // Called by $httpBackend whenever this mock's pattern is matched
+    var responder = mock.responder,
+      responseData = mock.responseData;
+
+    // If the mock says to authenticate and we don't have
+    // an Authorization header, return 401.
+    if (mock.authenticate) {
+      var authz = headers['Authorization'];
+      if (!authz) {
+        return [401, {"message": "Login required"}];
+      }
+    }
+
+    // A generic responder for handling the case where the
+    // mock just wanted the basics and supplied responseData
+    if (!responder) {
+      return [200, responseData]
+    }
+
+    // Got here, so let's go ahead and call the
+    // registered responder
+    return responder(method, url, data, headers)
+  }
 }
 
 
