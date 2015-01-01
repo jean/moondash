f83f7851251249fd8ab844c4f7aef8e51279fb38
Paul Everitt <paul@agendaless.com>
2015-01-01 13:12:06 -0500

    Not good test coverage, but onwards.

--- src/module.js
+++ src/module.js
@@ -2,7 +2,7 @@
 
 var dependencies = [
   // Our submodules
-  'md.common', 'md.config', 'md.layout',
+  'md.common', 'md.config', 'md.layout', 'md.mockapi',
   'md.auth', 'md.forms', 'md.nav', 'md.dispatch', 'md.resourcetypes',
 
   // External stuff
