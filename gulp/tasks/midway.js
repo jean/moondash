4729ca6b55dbf39f38385ffa14df29f97cb28ae2
Eric BREHAULT <ebrehault@gmail.com>
2015-01-04 15:29:52 +0100

    do not call build task from midway task

--- gulp/tasks/midway.js
+++ gulp/tasks/midway.js
@@ -3,7 +3,7 @@ var
   karma = require('karma-as-promised'),
   testConf = require('../config').midway;
 
-gulp.task('midway', ['build'], function () {
+gulp.task('midway', function () {
   return karma.server
     .start({
              configFile: __dirname + '/' + testConf.karma,
