dca7865c9d6a38acbd720114fd918dfea61ac5ef
Paul Everitt <paul@agendaless.com>
2015-01-09 17:57:20 -0500

    Move the dispatcher to its own exportable function, to make it easier to test.

--- src/mockapi/providers.js
+++ src/mockapi/providers.js
@@ -3,6 +3,41 @@
 var _ = require('lodash');
 var url = require('url');
 
+function Dispatcher(mock, method, thisUrl, data, headers) {
+  // Called by $httpBackend whenever this mock's pattern is matched.
+
+  var responder, responseData, response, request, parsedUrl;
+
+  // If the mock says to authenticate and we don't have
+  // an Authorization header, return 401.
+  if (mock.authenticate) {
+    var authz = headers['Authorization'];
+    if (!authz) {
+      return [401, {'message': 'Login required'}];
+    }
+  }
+
+  responder = mock.responder;
+  responseData = mock.responseData;
+
+  // A generic responder for handling the case where the
+  // mock just wanted the basics and supplied responseData
+  if (responseData) {
+    response = [200, responseData]
+  } else {
+    // Package up request information into a convenient data,
+    // call the responder, and return the response.
+    request = url.parse(thisUrl, true);
+    request.url = thisUrl;
+    request.headers = headers;
+    request.data = data;
+    if (data) request.json_body = JSON.parse(data);
+    response = responder(request);
+  }
+
+  return response;
+}
+
 function MockRest() {
   var _this = this;
   this.mocks = [];
@@ -28,7 +63,7 @@ function MockRest() {
         pattern = mock.pattern;
 
       var wrappedResponder = function (method, url, data, headers) {
-        return dispatch(mock, method, url, data, headers);
+        return Dispatcher(mock, method, url, data, headers);
       };
 
       $httpBackend.when(method, pattern)
@@ -36,40 +71,6 @@ function MockRest() {
     });
   }
 
-  function dispatch(mock, method, thisUrl, data, headers) {
-    // Called by $httpBackend whenever this mock's pattern is matched.
-
-    var responder, responseData, response, request, parsedUrl;
-
-    // If the mock says to authenticate and we don't have
-    // an Authorization header, return 401.
-    if (mock.authenticate) {
-      var authz = headers['Authorization'];
-      if (!authz) {
-        return [401, {'message': 'Login required'}];
-      }
-    }
-
-    responder = mock.responder;
-    responseData = mock.responseData;
-
-    // A generic responder for handling the case where the
-    // mock just wanted the basics and supplied responseData
-    if (responseData) {
-      response = [200, responseData]
-    } else {
-      // Package up request information into a convenient data,
-      // call the responder, and return the response.
-      request = url.parse(thisUrl, true);
-      request.url = thisUrl;
-      request.headers = headers;
-      request.data = data;
-      if (data) request.json_body = JSON.parse(data);
-      response = responder(request);
-    }
-
-    return response;
-  }
 }
 
 
@@ -87,5 +88,6 @@ function ModuleRun($httpBackend, MdMockRest) {
 
 module.exports = {
   MockRest: MockRest,
-  Run: ModuleRun
+  Run: ModuleRun,
+  Dispatcher: Dispatcher
 };
--- src/mockapi/test/unit/providers.js
+++ src/mockapi/test/unit/providers.js
@@ -7,7 +7,7 @@ var
   stub = helper.stub,
   MockRest = require('../../providers').MockRest;
 
-describe('MockRest Provider', function () {
+describe('MockRest Provider Basics', function () {
 
   var mr;
 
@@ -31,8 +31,30 @@ describe('MockRest Provider', function () {
     expect(mr.mocks['someMocks'][0].id).to.equal(1);
   });
 
-  it('should register some mocks', function () {
+});
 
+describe('MockRest Dispatcher', function () {
+
+  var mr;
+
+  beforeEach(function () {
+    mr = new MockRest();
+  });
+
+  it('should have basic api', function () {
+    expect(mr.mocks).to.be.empty();
+    expect(mr.mocks).to.be.a('object');
+    expect(mr.$get).to.exist();
+    expect(mr.addMocks).to.exist();
+  });
+
+  it('should have a $get instantiator for Angular', function () {
+    expect(mr.$get().registerMocks).to.be.a('function');
+  });
+
+  it('should have add mocks', function () {
+    mr.addMocks('someMocks', [{id: 1}]);
+    expect(mr.mocks['someMocks'][0].id).to.equal(1);
   });
 
 });
