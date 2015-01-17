73ad5dc406c44deb942ad01642759e6c84fe0ef7
Paul Everitt <paul@agendaless.com>
2015-01-17 16:11:40 -0500

    Start of a Protractor test, under the component (rather than some global demo.)

--- src/mockapi/test/e2e/mock_resource_type.js
+++ src/mockapi/test/e2e/mock_resource_type.js
@@ -1,10 +1,10 @@
-describe('Hello teest', function() {
-  it('Hello moondash!', function() {
+describe('src/mockapi Test', function() {
+  it('Test moondash!', function() {
     browser.get('src/mockapi/test/e2e/mock_resource_type/index.html');
 
     var hello = element(by.css('h1'));
 
-    expect(hello.getText()).toEqual('Mock Resource Type');
+    expect(hello.getText()).toEqual('mockapi tests');
   });
 
 });
--- src/mockapi/test/e2e/mock_resource_type/app.js
+++ src/mockapi/test/e2e/mock_resource_type/app.js
@@ -1,6 +1,36 @@
-function HelloController () {
-    this.title = 'Mock Resource Type';
+function RootController(collectionLIST) {
+  this.responses = [
+    {title: 'collectionLIST', value: JSON.stringify(collectionLIST)}
+  ];
 }
 
-angular.module('hello', ['moondash'])
-  .controller('HelloController', HelloController);
+function ModuleConfig($stateProvider, MdMockRestProvider) {
+
+  // Register some mock data at /api/resources/invoices
+  var
+    invoices = {
+      invoice1: {id: "invoice1", title: 'First invoice'},
+      invoice2: {id: "invoice2", title: 'Second invoice'}
+    },
+    MockResourceType = MdMockRestProvider.MockResourceType,
+    invoicesMock = new MockResourceType('/api/resourcetypes', 'invoices', invoices);
+
+  MdMockRestProvider.addMocks(invoicesMock.listMocks());
+
+  // Make a root state that retrieves all the URLs and displays them
+  $stateProvider
+    .state("state1", {
+             url: '/',
+             templateUrl: 'root.html',
+             controller: RootController,
+             controllerAs: 'ctrl',
+             resolve: {
+               collectionLIST: function ($http) {
+                 return $http.get('/api/resourcetypes/invoices/items');
+               }
+             }
+           })
+}
+
+angular.module('app', ['moondash', 'ui.router'])
+  .config(ModuleConfig);
